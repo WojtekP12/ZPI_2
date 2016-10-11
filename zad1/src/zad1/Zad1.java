51beb48339dce7080a2fb4b8eaa5b3ab41b3294a
Revill1 <adamredlinski@gmail.com>
2016-10-11 11:06:46 +0200

    Dodanie pobierania parametrow od uzytkownika

--- zad1/src/zad1/Zad1.java
+++ zad1/src/zad1/Zad1.java
@@ -1,10 +1,21 @@
 package zad1;
 
+import java.util.Scanner;
+
 public class Zad1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String[] parameters = new String[3];
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Prosze podac nazwe pliku");
+		parameters[0] = input.next();
+		System.out.println("Prosze podac tresc 'poprawnego' ciagu znakow");
+		parameters[1] = input.next();
+		System.out.println("Prosze podac tresc 'niepoprawnego' ciagu znakow");
+		parameters[2] = input.next();
 	}
 
 }
